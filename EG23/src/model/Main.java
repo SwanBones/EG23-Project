bfbadfab76cb1a22d3ea2b2d1e3b5fdce4860ab4
boone <boone@host.docker.internal>
2023-06-08 19:06:37 +0200

    changed maps

--- EG23/src/model/Main.java
+++ EG23/src/model/Main.java
@@ -6,10 +6,16 @@ public class Main {
 	private static String player1Name;
 	private static String player2Name;
 	
-	private static Color beige = new Color(218, 179, 124);
-	private static Color lightBeige = new Color(243, 220, 188);
-	private static Color darkBlue = new Color(73, 95, 109);
-	private static Color lightBlue = new Color(222, 241, 255);
+	private static Color beige = new Color(203, 188, 154);
+	private static Color lightBeige = new Color(237, 225, 205);
+	private static Color darkBlue = new Color(164, 150, 123);
+	private static Color lightBlue = new Color(217, 209, 137);
+		
+	
+	//private static Color beige = new Color(218, 179, 124);
+	//private static Color lightBeige = new Color(243, 220, 188);
+	//private static Color darkBlue = new Color(73, 95, 109);
+	//private static Color lightBlue = new Color(222, 241, 255);
 	
 	public static Color getBeige() {
 		return beige;
